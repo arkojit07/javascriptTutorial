f0f02d8156c7d4d0fd576434461253f999596b77
Arkojit Kar <arkojit2410@gmail.com>
2025-06-22 14:55:16 +0530

    first commit

---
+++ math.js
@@ -0,0 +1,4 @@
+let a=4;
+let b=5;
+a+=4;//a=a+4
+console.log("a=",a);
---
+++ myfirst.js
@@ -0,0 +1,24 @@
+//js is dynamically type language
+//popup notification
+alert("hii")
+//use to print
+console.log("arkojit kar"); 
+//define variable
+let age=35
+let name="tony stark"
+//boolean type variable
+let isFollow=true;
+console.log(isFollow);
+//object
+const student={
+    fullName:"arkojit kar",
+    age:18,
+    cgpa:9.5,
+    isPass:true,
+};
+//change value from objects
+student["age"]=student["age"]+1;
+student["fullName"]="rohit sharma"
+//print any specific data from objects
+console.log(student.age);
+console.log(student["age"]); //another way
